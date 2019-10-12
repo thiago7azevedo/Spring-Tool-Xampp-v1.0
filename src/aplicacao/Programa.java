5669addb2d9635721efceb5d936baa1b76f38762
Thiago Azevedo <thiago7azevedo@gmail.com>
2019-10-12 12:32:24 -0300

    JPA-removendo dados/entidade monitorada

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -10,13 +10,16 @@ public class Programa {
 
 	public static void main(String[] args) {
 
+		//entidade monitorada
 	
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		Pessoa p = new Pessoa(2, null, null); // JPA busca uma pessoa do BD, converte em objeto e retorna
+		Pessoa p = em.find(Pessoa.class, 2); // objeto criado para recuperar o objeto do BD
+		em.getTransaction().begin(); //abre a conexão
+		em.remove(p); // remove o bojeto que foi buscado acima e colcoado na variavel p.
+		em.getTransaction().commit(); // fecha a conexão
 		
-		em.remove(p);
 		
 		System.out.println(p);
 		System.out.println("Pronto!");
